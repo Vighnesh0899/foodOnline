4b5d050ccdcef882d4073b68e1382ce79f5d60f2
Vighnesh0899 <vighneshwalawalkar9087@gmail.com>
2024-07-04 00:14:05 +0530

    customer and vendor registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,12 @@ urlpatterns = [
     path('registerUser/', views.registerUser, name='registeruser'),
     path('registerVendor/', views.registerVendor, name='registervendor'),
 
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('myAccount/', views.myAccount, name='myAccount'),
+    path('custDashboard/', views.custDashboard, name='custDashboard'),
+    path('vendorDashboard/', views.vendorDashboard, name='vendorDashboard'),
+
+
+
 ]
